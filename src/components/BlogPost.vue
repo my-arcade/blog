9a18fad05e4bf8342104767267ae586354925245
Kato Charles <kcharles52@gmail.com>
2024-05-14 13:24:03 +0300

    feat: blog post

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  props: ['post'],
+  setup(props) {
+    const post = ref(props.post);
+
+    watch(() => post.value.title, (newTitle, oldTitle) => {
+      if (newTitle!== oldTitle) {
+        // Update the post title
+      }
+    });
+
+    return { post };
+  }
+}
+</script>
---
+++ src/components/BlogPostList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BlogPost v-for="post in posts" :key="post.id" :post="post" />
+  </div>
+</template>
+
+<script>
+import BlogPost from './BlogPost.vue';
+
+export default {
+  components: {
+    BlogPost
+  },
+  props: ['posts']
+}
+</script>
